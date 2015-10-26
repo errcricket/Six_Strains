d28784dd0dc5a442745f77be507a1abd66fc535d
errcricket <work.err29@gmail.com>
2015-10-26 09:44:09 -0400

    updated filelist, original gb files had plasmids mixed with chromo. Split files, noted if plasmid or chromosome./

--- extractGB.py
+++ extractGB.py
@@ -4,7 +4,7 @@ Date:	10.30.2011
 Modification Date: 10.23.2015
 
 Purpose: To extact information from GenBank file and place the information to a new csv file. Output file should have the following format:
-ID\tBacteria\tStrain\tLocus_tag\tCDS_Region\tGene_info\tSequence\tProduct\n.
+Strain\tBacteria\tStrain\tLocus_tag\tCDS_Region\tGene_info\tSequence\tProduct\n.
 '''
 
 import Bio
@@ -22,7 +22,8 @@ from Bio.SeqFeature import Reference, SeqFeature, FeatureLocation
 from Bio.Alphabet import IUPAC
 from StringIO import StringIO
 
-fileList = ['Campy1147c/Campy1147c.gbf', 'Campy14076c/Campy14076c.gbf', 'Campy3194c/Campy3194c.gbf', 'Campy1246c/Campy1246c.gbf', 'Campy1147q/Campy1147q.gbf', 'Campy1285c/Campy1285c.gbf', 'Campy1188c/Campy1188c.gbf']
+fileList = ['Campy1147c/Campy1147c_chrom.gbf', 'Campy1147q/Campy1147q_Chrom_1.gbf', 'Campy1147q/Campy1147q_Chrom_2.gbf', 'Campy1147q/Campy1147q_Chrom_3.gbf', 'Campy1188c/Campy1188c_Chrom.gbf', 'Campy1188c/Campy1188c_Plasmid.gbf', 'Campy1246c/Campy1246c_Chrom.gbf', 'Campy1246c/Campy1246c_Plasmid.gbf', 'Campy1285c/Campy1285c_Chrom.gbf', 'Campy14076c/Campy14076c_Chrom.gbf', 'Campy3194c/Campy3194c_Chrom.gbf', 'Campy3194c/Campy3194c_Plasmid.gbf']
+#fileList = ['Campy1147c/Campy1147c.gbf', 'Campy14076c/Campy14076c.gbf', 'Campy3194c/Campy3194c.gbf', 'Campy1246c/Campy1246c.gbf', 'Campy1147q/Campy1147q.gbf', 'Campy1285c/Campy1285c.gbf', 'Campy1188c/Campy1188c.gbf']
 campy_dic = {}
 
 fileCounter = 0 #will increment +1 for each loop & will be the unique number
@@ -39,9 +40,18 @@ def strip_it(string_name):
 for f in fileList:
 	nameOut = f.replace('gbf', 'txt')
 	file_name = f.split('/')[0]
+	print(file_name)
+	dna_type = f.split('_')[1]
+	print(file_name, dna_type)
+	
+	if dna_type.startswith('C'):
+		dna_type = 'Chromosome'
+	else:
+		dna_type = 'Plasmid'
+	
 	with open(f, 'r') as inputFile:
 		with open(nameOut, 'w') as outputFile:
-			outputFile.write('FileName\tLocus_Tag\tProduct\tProtein_ID\tStrand\tTransl_Tbl\tSeq_AA\n')
+			outputFile.write('Strain\tDNA_Source\tLocus_Tag\tProduct\tProtein_ID\tStrand\tTransl_Tbl\tSeq_AA\n')
 
 			record = SeqIO.parse(f, 'genbank').next()
 
@@ -49,7 +59,7 @@ for f in fileList:
 			for f in record.features:
 				if f.type == 'CDS':
 					CDS = record.features[featureCount]
-					string = file_name #nameOut.replace('txt', 'gbf')
+					string = file_name + '\t' + dna_type 
 
 					for q in qualies:
 						if q in CDS.qualifiers:
@@ -65,11 +75,12 @@ for f in fileList:
 
 #This creates the corpus file
 with open('campy6_corpus_cds.txt', 'w') as outputFile:
-	outputFile.write('FileName\tLocus_Tag\tProduct\tProtein_ID\tStrand\tTransl_Tbl\tSeq_AA\n')
-	fileList = ['Campy1147c/Campy1147c.txt', 'Campy14076c/Campy14076c.txt', 'Campy3194c/Campy3194c.txt', 'Campy1246c/Campy1246c.txt', 'Campy1147q/Campy1147q.txt', 'Campy1285c/Campy1285c.txt', 'Campy1188c/Campy1188c.txt']
+	outputFile.write('Strain\tDNA_Source\tLocus_Tag\tProduct\tProtein_ID\tStrand\tTransl_Tbl\tSeq_AA\n')
+	fileList = ['Campy1147c/Campy1147c_chrom.txt', 'Campy1147q/Campy1147q_Chrom_1.txt', 'Campy1147q/Campy1147q_Chrom_2.txt', 'Campy1147q/Campy1147q_Chrom_3.txt', 'Campy1188c/Campy1188c_Chrom.txt', 'Campy1188c/Campy1188c_Plasmid.txt', 'Campy1246c/Campy1246c_Chrom.txt', 'Campy1246c/Campy1246c_Plasmid.txt', 'Campy1285c/Campy1285c_Chrom.txt', 'Campy14076c/Campy14076c_Chrom.txt', 'Campy3194c/Campy3194c_Chrom.txt', 'Campy3194c/Campy3194c_Plasmid.txt']
 	for f in fileList:
 		with open(f, 'r') as inputFile:
 			inputFile.readline()
 			lines = inputFile.readlines()
 			for line in lines:	
 				outputFile.write(line)
+
