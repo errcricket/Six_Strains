44cb037b4f8e79d5894a1b395190381222d63b9d
errcricket <errcricket@users.noreply.github.com>
2015-10-28 07:28:22 -0400

    verify this copy w/ work copy

--- r_campy6.R
+++ r_campy6.R
@@ -16,11 +16,11 @@ myDF <- read.csv('campy6_corpus_cds.txt', header=T, sep='\t')
 print(names(myDF))
 print(length(names(myDF)))
 
-#[1] "FileName"   "Locus_Tag"  "Product"    "Protein_ID" "Strand"     "Transl_Tbl" "Seq_AA"    
+#[1] "Filename"   "Locus_Tag"  "Product"    "Protein_ID" "Strand"     "Transl_Tbl" "Seq_AA"    
 
 png(filename=paste('Histogram.png', sep=''), width=750,height=550,res=72)
 par(mar=c(9.5,4.3,4,2))
-print(h <- ggplot(myDF, aes(x=FileName, stat='bin')) + geom_bar() +
+print(h <- ggplot(myDF, aes(x=Filename, stat='bin')) + geom_bar() +
 		labs(title='Gene Count by Campy Strain', x='Campy. Strain', y='Gene Count\n') +
     	guides(title.theme = element_text(size=15, angle = 90)) + theme(legend.text=element_text(size=13)) +
 		theme(axis.text.x=element_text(angle=90, size=14, hjust=1)) )
@@ -35,6 +35,6 @@ print(h <- ggplot(myDF, aes(x=FileName, stat='bin')) + geom_bar() +
 uLocus = sort(unique(unlist(myDF$Locus_Tag, use.names = FALSE)))
 png(filename=paste('XHistogram.png', sep=''), width=750,height=550,res=72)
 par(mar=c(9.5,4.3,4,2))
-print(x <- qplot(factor(FileName), data=myDF, geom='bar', fill=factor(length(uLocus))))
-#print(x <- qplot(factor(FileName), data=myDF, geom='bar', fill=factor(Locus_Tag)))
+print(x <- qplot(factor(Strain), data=myDF, geom='bar', fill=factor(length(uLocus))))
+#print(x <- qplot(factor(Filename), data=myDF, geom='bar', fill=factor(Locus_Tag)))
 dev.off()
