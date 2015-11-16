1f99e09a811f9ba6ea29d0afb4053418d25f7542
errcricket <work.err29@gmail.com>
2015-11-16 11:47:41 -0500

    changed inputfile, removed excess lines

--- generate_Jaccard_heatMaps.r
+++ generate_Jaccard_heatMaps.r
@@ -7,16 +7,16 @@ library(reshape2)
 original.parameters=par()
 options(width=9999)
 
-myDF <- read.csv('Output/jaccard_r.txt', header=T, sep='\t')
-myDF$Jaccard_C <- round(100*myDF$Jaccard_C, 2)
+myDF <- read.csv('Output/jaccard_statistics.txt', header=T, sep='\t') 
+#myDF <- read.csv('Output/jaccard_statistics.txt', header=T, sep='\t', skip=146) #note, deleted initial lines
+
+myDF$Jaccard_C <- round(100*(as.numeric(myDF$Jaccard_C)), 2)
 
 c_all = melt(myDF)
-print(c_all)
-
+#print(c_all)
 
 png(filename=paste('images/Jaccard_Heatmap.png', sep=''), width=3750,height=2750,res=300)
 par(mar=c(9.5,4.3,4,2))
-#print(corpus <- qplot(x=Strain1, y=Strain2, data=c_all, fill=value, geom='tile', colour='black', name=NULL) +
 print(corpus <- qplot(x=Strain1, y=Strain2, data=c_all, fill=value, geom='tile') +
                geom_text(aes(label=value), color='black', size=4) +
 					scale_fill_gradient(low='cadetblue4', high='seagreen2', limit=c(80,100)) +
