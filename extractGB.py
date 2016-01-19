df951ac4506791517982a51fb8fe8cf100bd3990
errcricket <work.err29@gmail.com>
2016-01-19 11:47:57 -0500

    moved renaming portion of code s.t. there is no f at end of name

--- extractGB.py
+++ extractGB.py
@@ -26,7 +26,7 @@ def strip_it(string_name):
 
 ###############################################File List & Searchable Qualifiers###################################################
 fileList = ['Campy1147c/Campy1147c_Chrom.gbf', 'Campy1147q/Campy1147q_Chrom_1.gbf', 'Campy1147q/Campy1147q_Chrom_2.gbf', 'Campy1147q/Campy1147q_Chrom_3.gbf', 'Campy1188c/Campy1188c_Chrom.gbf', 'Campy1188c/Campy1188c_Plasmid.gbf', 'Campy1246c/Campy1246c_Chrom.gbf', 'Campy1246c/Campy1246c_Plasmid.gbf', 'Campy1285c/Campy1285c_Chrom.gbf', 'Campy14076c/Campy14076c_Chrom.gbf', 'Campy3194c/Campy3194c_Chrom.gbf', 'Campy3194c/Campy3194c_Plasmid.gbf', 'CampyCP006702/CP006702_Chrom.gb', 'CampyCP007179/CP007179_Chrom.gb', 'CampyCP007181/CP007181_Chrom.gb']
-qualies = ['locus_tag', 'product', 'protein_id', 'note', 'transl_table', 'translation']
+qualies = ['locus_tag', 'product', 'transl_table', 'note', 'translation', 'protein_id']
 #---------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -34,15 +34,16 @@ qualies = ['locus_tag', 'product', 'protein_id', 'note', 'transl_table', 'transl
 for f in fileList:
 	extension = os.path.splitext(f)[1]
 	nameOut = ''
+	file_name = ''
 	if extension == '.gbf':
 		nameOut = f.replace('gbf', 'txt')
+		file_name = f.split('/')[1].replace('.gbf', '')
 	else:
 		nameOut = f.replace('gb', 'txt') #not all files are gbf extension. Be sure to keep after above as replacing gb <- txtf
+		file_name = f.split('/')[1].replace('.gb', '')
 	strain = f.split('/')[0]
-	file_name = f.split('/')[1].replace('.gbf', '')
-	file_name = f.split('/')[1].replace('.gb', '')
 	dna_type = f.split('_')[1]
-	print(file_name)
+	print(nameOut, file_name)
 	
 	if dna_type.startswith('C'):
 		dna_type = 'Chromosome'
@@ -52,10 +53,10 @@ for f in fileList:
 	string = ''
 	with open(f, 'r') as handle:
 		with open(nameOut, 'w') as outputFile:
-			outputFile.write('\t'.join(['Filename', 'Strain', 'DNA_Source', 'Locus_Tag', 'Product', 'Protein_ID', 'Note', 'Transl_Tbl', 'Seq_AA']) + '\n')
+			outputFile.write('\t'.join(['Filename', 'Strain', 'DNA_Source', 'Locus_Tag', 'Product', 'Transl_Tbl', 'Note', 'Seq_AA', 'Protein_ID']) + '\n')
+											#[1] 'Filename'   'Strain' 'DNA_Source' 'Locus_Tag'  'Product'  'Transl_Tbl'  'Note'  'Seq_AA'  'Protein_ID'
 
 			for record in SeqIO.parse(handle, 'genbank'):
-				print(len(record.seq))
 				for feature in record.features:
 					if feature.type=='CDS':
 						cds_dic = {} 
@@ -71,10 +72,11 @@ for f in fileList:
 						outputFile.write(string + '\n') # print(feature, string)
 #---------------------------------------------------------------------------------------------------------------------------------
 
-#This creates the corpus file
+
 ############################Create Corpus File (from individual text files)####################################
 with open('Output/campy6_corpus_cds.txt', 'w') as outputFile:
-	outputFile.write('\t'.join(['Filename', 'Strain', 'DNA_Source', 'Locus_Tag', 'Product', 'Protein_ID', 'Note', 'Transl_Tbl', 'Seq_AA']) + '\n')
+	outputFile.write('\t'.join(['Filename', 'Strain', 'DNA_Source', 'Locus_Tag', 'Product', 'Transl_Tbl', 'Note', 'Seq_AA', 'Protein_ID']) + '\n')
+	#outputFile.write('\t'.join(['Filename', 'Strain', 'DNA_Source', 'Locus_Tag', 'Product', 'Protein_ID', 'Note', 'Transl_Tbl', 'Seq_AA']) + '\n')
 	fileList = ['Campy1147c/Campy1147c_Chrom.txt', 'Campy1147q/Campy1147q_Chrom_1.txt', 'Campy1147q/Campy1147q_Chrom_2.txt', 'Campy1147q/Campy1147q_Chrom_3.txt', 'Campy1188c/Campy1188c_Chrom.txt', 'Campy1188c/Campy1188c_Plasmid.txt', 'Campy1246c/Campy1246c_Chrom.txt', 'Campy1246c/Campy1246c_Plasmid.txt', 'Campy1285c/Campy1285c_Chrom.txt', 'Campy14076c/Campy14076c_Chrom.txt', 'Campy3194c/Campy3194c_Chrom.txt', 'Campy3194c/Campy3194c_Plasmid.txt', 'CampyCP006702/CP006702_Chrom.txt', 'CampyCP007179/CP007179_Chrom.txt', 'CampyCP007181/CP007181_Chrom.txt']
 	for f in fileList:
 		with open(f, 'r') as inputFile:
