e92a1c0b2320c60606856c7a156560acfcefbb0e
errcricket <work.err29@gmail.com>
2016-01-26 16:47:50 -0500

    cleaning up code

--- Genome_Alignment/compare_strainOrthologs.R
+++ Genome_Alignment/compare_strainOrthologs.R
@@ -2,21 +2,17 @@
 
 options(warn=1)
 
-#library('data.table')
 library(plyr)
 library(ggplot2)
 library(reshape2)
-#library(reshape)
 library(scales)
-#require(utils)
 library(dplyr)
 require(RColorBrewer)
 
 original.parameters=par()
 options(width=9999)
 
-myDF <- read.csv('sequenceIdentity.txt', header=T, sep='\t') 
-#print(names(myDF)) #[1] 'Ortholog_strain1'    'Ortholog_strain2'    'ortholog_similarity' 'Region_strain1'      'Region_strain2'     
+myDF <- read.csv('sequenceIdentity.txt', header=T, sep='\t') #print(names(myDF)) #[1] 'Ortholog_strain1'    'Ortholog_strain2'    'ortholog_similarity'
 myDF <- myDF[1:3] #only interested in first 3 columns
 
 means.df = ddply(myDF, .(Ortholog_strain1, Ortholog_strain2), summarize, averageIdentity=round(mean(ortholog_similarity, na.rm=TRUE), 2))
@@ -30,7 +26,7 @@ print(c_all)
 
 ##PRINT HEATMAP OF STRAIN IDENTITY SIMILARITY:
 #--------------------------------------------------------------------------
-png(filename=paste('seqIdentity.png', sep=''), width=3750,height=2750,res=300)
+png(filename=paste('../images/seqIdentity.png', sep=''), width=3750,height=2750,res=300)
 par(mar=c(9.5,4.3,4,2))
 print(corpus <- qplot(x=Ortholog_strain1, y=Ortholog_strain2, data=c_all, fill=value, geom='tile') +
 					geom_text(aes(label=value), color='black', size=4) + 
